32fac3c2b711fe0beb7a2fde398ab38bfed52093
Vinod Kakumani <vinodkakumani@outlook.com>
2018-07-22 21:35:29 +0530

    header changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 // Shared Service Module - All provider imports will go in this module
 // No other module is allowed to import this module.
 import { ServiceModule } from './modules/service/service.module';
 
-// App Routing Module
-import { AppRoutingModule } from './app-routing.module';
 import { LoginModule } from './modules/feature/login/login.module';
 import { RegistrationModule } from './modules/feature/registration/registration.module';
 import { DashboardModule } from './modules/feature/dashboard/dashboard.module';
 import { MaterialExamplesModule } from './modules/feature/material-examples/material-examples.module';
+
+// App Routing Module
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
@@ -20,14 +22,15 @@ import { ChatboxComponent } from './components/shared/chatbox/chatbox.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
     HeaderComponent,
     SidebarComponent,
-    ChatboxComponent
+    ChatboxComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
     ServiceModule,
     LoginModule,
     RegistrationModule,
